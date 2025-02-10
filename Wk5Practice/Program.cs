9df6eb0f643fb5056f7c66c20d88aea473035d44
Jose Villa <villa1997.jv@gmail.com>
2025-02-10 14:06:04 -0600

    Add project files.

---
+++ Wk5Practice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Wk5Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask user input a string
+            string input = Console.ReadLine();
+            //process
+            Dictionary<char, int> duplications = new Dictionary<string, int>();
+            foreach (char c in input)
+            {
+                //check if dictionary already not has a key (character), then add a key
+                if (!duplications.ContainsKey(c))
+                {
+                    duplications[c] = 1;
+                }
+                else
+                {
+                    duplications[c]++;
+                }
+            }
+
+            foreach (KeyValuePair<string, int> pair in duplications.Values)
+            { 
+                Console.WriteLine(Kvp.key + ";"+kvp.Value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Wk5Practice/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Wk5Practice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Wk5Practice")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c0e40e1c-55c7-4d4e-bd33-2cdf8573588f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
